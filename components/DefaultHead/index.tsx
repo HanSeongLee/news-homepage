dd2c4b7a3453f84dbe71c7ee0fed3d799a04f247
HanSeongLee <jkhanseong@gmail.com>
2023-03-23 18:50:09 +0900

    OPTIMIZE | Google fonts performance

--- components/DefaultHead/index.tsx
+++ components/DefaultHead/index.tsx
@@ -56,10 +56,12 @@ const DefaultHead: React.FC<IProps> = ({ title, description, url, ogi }) => {
                   content={ogi}
             />
             <link rel="icon" type="image/png" sizes="32x32" href="/favicon.png" />
-            <link rel="preconnect" href="https://fonts.googleapis.com" />
-            <link rel="preconnect" href="https://fonts.gstatic.com" />
+            <link rel="preconnect" href="https://fonts.googleapis.com" crossOrigin="anonymous" />
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
             <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700;800&display=swap"
-                  rel="stylesheet"
+                  rel="preload"
+                  as="style"
+                  crossOrigin="anonymous"
             />
         </>
     );
